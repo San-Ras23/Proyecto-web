61846eafa49b9d35dd42eb97b01614bf83e74911
Sandra Rasgido <145142989+San-Ras23@users.noreply.github.com>
2023-11-18 13:04:22 -0300

    arreglos href y API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@ formulario.addEventListener ('submit' , async(e) => {
     const respuesta = await fetch ('https://sheet.best/api/sheets/9e2b910a-a773-4f7d-b1b2-c9fafb21efe1', {
         method: 'POST',
         mode: 'cors',
-        header: {
+        headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify ({
